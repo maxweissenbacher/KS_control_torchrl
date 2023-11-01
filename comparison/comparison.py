cbfe6372b8bba9511d1ee855478d0dc6d0638785
maxweissenbacher <max.weissenbacher@proton.me>
2023-11-01 11:57:20 +0000

    Added code to compare performance of different algos

---
+++ comparison/comparison.py
@@ -0,0 +1,28 @@
+from ablation_study.ablation_study_analysis import extract_logs
+import pathlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    study_path = pathlib.Path('baseline')
+    rewards = []
+    for directory in study_path.iterdir():
+        if directory.is_dir():
+            logs, config = extract_logs(directory)
+            rewards.append(np.mean(logs['eval/reward'][-5:]))
+
+    print(config['env'])
+    print(rewards)
+
+    # to add more plots, just pass a list of the arrays/lists to use in the boxplots
+    all_data = [rewards, rewards]
+    labels = ['baseline', 'lstm']
+
+    plt.boxplot(all_data)
+    plt.xticks([x+1 for x in range(len(all_data))], labels=labels)
+    plot_title = "Performance comparison for "
+    plot_title += f"nu={config['env']['nu']} | "
+    plot_title += f"a={config['env']['num_actuators']} | "
+    plot_title += f"s={config['env']['num_sensors']}"
+    plt.title(plot_title)
+    plt.show()
